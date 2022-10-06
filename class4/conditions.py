ac63700d4ed8b81ecbf4dc4fd62be91de4322da9
somename <a@gmail.com>
2022-10-06 17:29:54 +0000

    added new

---
+++ class4/conditions.py
@@ -0,0 +1,70 @@
+# #in
+
+# HIDDEN_FRUIT = "kiwi"
+
+# FRUIT_LIST1 = ["apple", "pear", "banana"]
+# print(type(FRUIT_LIST1))
+# FRUIT_LIST2 = ("mango", "melon", "cherry")
+# print(type(FRUIT_LIST2))
+# FRUIT_LIST3 = {"peach", "apricot", "kiwi"}
+# print(type(FRUIT_LIST3))
+
+# if HIDDEN_FRUIT in FRUIT_LIST1:
+#     print("Hidden fruit is in List1")
+# elif HIDDEN_FRUIT in FRUIT_LIST2:
+#     print("Hidden fruit is in List2")
+# elif HIDDEN_FRUIT in FRUIT_LIST3:
+#     print("Hidden fruit is in List3")
+# else:
+#     print("Hidden fruit is not found")
+
+# while True:
+#     try:
+#         RESPONSE = int(input("when were you born?: "))
+       
+#         if RESPONSE >= 2000 and RESPONSE <= 2022:
+#             print("you were born in 21st century")
+#         elif RESPONSE >= 1900 and RESPONSE <= 1999:
+#             print("you were born in 20th century")
+#         elif RESPONSE == 1899 or RESPONSE <= 1800:
+#             print("you should not be running this")
+#         else:
+#             print("something went wrong")    
+#         break
+#     except:
+#         print("Error:did you provide digit e.g 1")
+
+
+
+# []
+# COMMAND +  test
+# COMMAND - test
+# COMMAND +  test
+
+
+# while True:
+#     try:
+#         RESPONSE = int(input("when were you born?: "))
+       
+#         if RESPONSE >= 2000 and RESPONSE <= 2022:
+#             print("you were born in 21st century")
+#         elif RESPONSE >= 1900 and RESPONSE <= 1999:
+#             print("you were born in 20th century")
+#         elif RESPONSE == 1899 or RESPONSE <= 1800:
+#             print("you should not be running this")
+#         else:
+#             print("something went wrong")    
+#         break
+#     except TypeError
+#         print("Error:did you provide digit e.g 1")
+#     except ValueError
+#         print("Error:did you enter the right value?")
+
+
+
+ANSWER = "no"
+while ANSWER != "yes":
+     ANSWER = input("are you crazy?")
+     if ANSWER == "yes":
+        print("i knew that!")
+        
---
+++ class4/for_loop.py
@@ -0,0 +1,36 @@
+# for items in range(10):
+#     print(items)
+
+
+
+
+
+
+
+
+# USERS = ["tom", "ken", "lisa"]
+# for user_list in USERS:
+#     print(user_list)
+
+
+# USERS = ["tom", "ken", "lisa"]
+# for user_list in USERS:
+#     if user_list == "ken":
+#         print(user_list)
+
+# file1 = open("/etc/passwd")
+# for lines in file1:
+#     print(lines)
+
+# Open file1
+# file1 = open("/etc/passwd")
+# for lines in file1:
+#     print(lines.strip())
+
+# Close file1
+# file1.close()    
+    
+ #reserved words can't use them   
+ # if , for ,line , in ,and ,or, while,file, open, print   
+
+
